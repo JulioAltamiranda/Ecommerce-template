1e4791ac004853c2af685637b68937d2f371d2a4
JulioAltamiranda <julioaltamiranda77@gmail.com>
2020-10-22 13:55:51 -0300

    updated styles.scss,response.scss,_stylesDefault.scss, Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,7 +8,7 @@
             <div class="section-title">
                 <h2><span>Our</span>Categories</h2>
             </div>
-            <div class="category-img">
+            <div class="area">
                 <div class="single-category">
                     <a href="" class="category-link">
                         <h3>Tv & audio</h3>
@@ -40,7 +40,7 @@
             <div class="section-title">
                 <h2><span>Our</span>Products</h2>
             </div>
-            <div class="product-area">
+            <div class="area">
                 <div class="product-item">
                     <img src="@/assets/img/product1.jpg" alt="product1">
                     <div class="product-desc">
@@ -206,100 +206,97 @@
             <div class="section-title">
                 <h2><span>Best</span>Sellers</h2>
             </div>
-            <div class="best-content">
-                <div class="product-area">
-                    <div class="product-item">
-                        <img src="@/assets/img/product5.jpg" alt="product5">
-                        <div class="product-desc">
-                            <div class="product-name">Samsung j3 2016</div>
-                            <div>
-                                <span class="product-price">$ 20.50</span>
-                                <span class="old-price"></span>
-                            </div>
-                            <div class="product-rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </div>
+            <div class="area">
+                <div class="product-item">
+                    <img src="@/assets/img/product5.jpg" alt="product5">
+                    <div class="product-desc">
+                        <div class="product-name">Samsung j3 2016</div>
+                        <div>
+                            <span class="product-price">$ 20.50</span>
+                            <span class="old-price"></span>
                         </div>
-                        <div class="product-header">
+                        <div class="product-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
                         </div>
-                        <div class="product-hover">
-                        <a href="" class="heart-link" title="heart link">
-                            <i class="ti-heart"></i>
-                        </a>
-                        <a href="" class="cart">
-                            <i class="ti-shopping-cart-full" title="cart link"></i>   
-                        </a>
-                        <a href="" class="eye-link" title="eye link">
-                            <i class="ti-eye"></i>
-                        </a>
                     </div>
+                    <div class="product-header">
                     </div>
-                    <div class="product-item">
-                        <img src="@/assets/img/product5.jpg" alt="product5">
-                        <div class="product-desc">
-                            <div class="product-name">Samsung j3 2016</div>
-                            <div>
-                                <span class="product-price">$ 20.50</span>
-                                <span class="old-price"></span>
-                            </div>
-                            <div class="product-rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </div>
+                    <div class="product-hover">
+                    <a href="" class="heart-link" title="heart link">
+                        <i class="ti-heart"></i>
+                    </a>
+                    <a href="" class="cart">
+                        <i class="ti-shopping-cart-full" title="cart link"></i>   
+                    </a>
+                    <a href="" class="eye-link" title="eye link">
+                        <i class="ti-eye"></i>
+                    </a>
+                </div>
+                </div>
+                <div class="product-item">
+                    <img src="@/assets/img/product5.jpg" alt="product5">
+                    <div class="product-desc">
+                        <div class="product-name">Samsung j3 2016</div>
+                        <div>
+                            <span class="product-price">$ 20.50</span>
+                            <span class="old-price"></span>
                         </div>
-                        <div class="product-header">
-                            <span class="new">New</span>
+                        <div class="product-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
                         </div>
-                        <div class="product-hover">
-                        <a href="" class="heart-link" title="heart link">
-                            <i class="ti-heart"></i>
-                        </a>
-                        <a href="" class="cart">
-                            <i class="ti-shopping-cart-full" title="cart link"></i>   
-                        </a>
-                        <a href="" class="eye-link" title="eye link">
-                            <i class="ti-eye"></i>
-                        </a>
                     </div>
+                    <div class="product-header">
+                        <span class="new">New</span>
                     </div>
-                    <div class="product-item">
-                        <img src="@/assets/img/product5.jpg" alt="product5">
-                        <div class="product-desc">
-                            <div class="product-name">Samsung j3 2016</div>
-                            <div>
-                                <span class="product-price">$ 20.50</span>
-                                <span class="old-price"></span>
-                            </div>
-                            <div class="product-rating">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </div>
+                    <div class="product-hover">
+                    <a href="" class="heart-link" title="heart link">
+                        <i class="ti-heart"></i>
+                    </a>
+                    <a href="" class="cart">
+                        <i class="ti-shopping-cart-full" title="cart link"></i>   
+                    </a>
+                    <a href="" class="eye-link" title="eye link">
+                        <i class="ti-eye"></i>
+                    </a>
+                </div>
+                </div>
+                <div class="product-item">
+                    <img src="@/assets/img/product5.jpg" alt="product5">
+                    <div class="product-desc">
+                        <div class="product-name">Samsung j3 2016</div>
+                        <div>
+                            <span class="product-price">$ 20.50</span>
+                            <span class="old-price"></span>
                         </div>
-                        <div class="product-header">
+                        <div class="product-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="far fa-star"></i>
                         </div>
-                        <div class="product-hover">
-                        <a href="" class="heart-link" title="heart link">
-                            <i class="ti-heart"></i>
-                        </a>
-                        <a href="" class="cart">
-                            <i class="ti-shopping-cart-full" title="cart link"></i>   
-                        </a>
-                        <a href="" class="eye-link" title="eye link">
-                            <i class="ti-eye"></i>
-                        </a>
                     </div>
+                    <div class="product-header">
                     </div>
-                
+                    <div class="product-hover">
+                    <a href="" class="heart-link" title="heart link">
+                        <i class="ti-heart"></i>
+                    </a>
+                    <a href="" class="cart">
+                        <i class="ti-shopping-cart-full" title="cart link"></i>   
+                    </a>
+                    <a href="" class="eye-link" title="eye link">
+                        <i class="ti-eye"></i>
+                    </a>
+                </div>
                 </div>
                 <div class="ads">
                     <img src="@/assets/img/ads-06.jpg" alt="ads">
@@ -327,7 +324,7 @@
                 <div class="section-title">
                     <h2><span>Featured</span>Products</h2>
                 </div>
-                <div class="product-area">
+                <div class="area featured-content">
                     <div class="product-item">
                         <img src="@/assets/img/product5.jpg" alt="product5">
                         <div class="product-desc">
@@ -377,16 +374,16 @@
                         <div class="product-header">
                         </div>
                         <div class="product-hover">
-                        <a href="" class="heart-link" title="heart link">
-                            <i class="ti-heart"></i>
-                        </a>
-                        <a href="" class="cart">
-                            <i class="ti-shopping-cart-full" title="cart link"></i>   
-                        </a>
-                        <a href="" class="eye-link" title="eye link">
-                            <i class="ti-eye"></i>
-                        </a>
-                    </div>
+                            <a href="" class="heart-link" title="heart link">
+                                <i class="ti-heart"></i>
+                            </a>
+                            <a href="" class="cart">
+                                <i class="ti-shopping-cart-full" title="cart link"></i>   
+                            </a>
+                            <a href="" class="eye-link" title="eye link">
+                                <i class="ti-eye"></i>
+                            </a>
+                        </div>
                     </div>
                     <div class="product-item">
                         <img src="@/assets/img/product5.jpg" alt="product5">
