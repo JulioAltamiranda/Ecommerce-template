84764c061b0c7846a01840e50b870a0e5725bb94
JulioAltamiranda <julioaltamiranda77@gmail.com>
2020-10-22 09:45:39 -0300

    added loader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <master>
+  <master :show="show">
     <template slot="header">
       <headerComponent/>
     </template>
@@ -24,18 +24,21 @@ export default {
     headerComponent,
     mainComponent,
     footerComponent
+  },
+  data(){
+    return{
+      show:true
+    }
+  },
+  mounted(){
+    window.addEventListener('load',this.loading)
+  },
+  methods:{
+    loading(){
+      this.show=false
+    }
   }
  
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/layout/Master.vue
+++ src/components/layout/Master.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="wrapper">
+        <!-- Loader -->
+        <transition name="fade">
+            <div class="loader-container" v-show="show">
+                <div class="loader"></div>
+            </div>
+        </transition>
         <slot name="header"></slot>
         <slot name="main"></slot>
         <slot name="footer"></slot>
@@ -11,3 +17,8 @@
 <style lang="scss" src="@/assets/sass/responsive.scss">
 
 </style>
+<script>
+export default{
+    props:['show']
+}
+</script>
